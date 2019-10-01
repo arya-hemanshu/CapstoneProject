5c945ed86145fa4eb7b1e2a948ba7f5641d8ba27
hemanshu <arya.hemanshu@gmail.com>
2019-10-01 15:49:08 +0100

    fixed bug

--- create_infra.sh
+++ create_infra.sh
@@ -2,8 +2,6 @@
 
 UUID="$(uuidgen)"
 
-aws cloudformation create-stack \
---stack-name capstoneudacity$UUID \
---template-body capstone_infra.yml \
---parameters capstone_infra_parameter.json \
---region=eu-west-1
+aws cloudformation create-stack --stack-name capstoneudacity$UUID --template-body capstone_infra.yml \
+--parameters capstone_infra_parameter.json --region=eu-west-1
+
